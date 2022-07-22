eee5d706a921a1b912347ef61fa59c683360994f
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-07-22 23:54:43 +0100

    Js.const

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-axios.get('http://google.com')
-    .then(({ data }) => {
-        console.log(data);
-        for (let google of data.results) {
-            console.log(google.name);
-        }
-        console.log(google.next);
-    });
+// axios.get('http://google.com')
+//     .then(({ data }) => {
+//         console.log(data);
+//         for (let google of data.results) {
+//             console.log(google.name);
+//         }
+//         console.log(google.next);
+//     });
+
+const weatherIcon = document.querySelector('.weather-icon');
+const tempDescription = document.querySelector('.temperature-description');
+const tempValue = document.querySelector('.temperature-value');
+const location = document.querySelector('.location');
+
+
