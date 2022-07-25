e4a8ae25982b8c747ab5dc9f2225ae1de9bace5e
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-07-25 15:08:42 +0100

    all functions

--- app.js
+++ app.js
@@ -28,5 +28,44 @@ if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(setPosition, showError);
 } else {
     notificationElement.style.display = 'block';
-    notificationElement.innerHTML = "<p> Doesn't support Maps</p>";
+    notificationElement.innerHTML = `<p> Doesn't support Maps</p>`;
+}
+
+function setPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+
+function showError(error) {
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML = `<p> ${error.message}`;
+}
+
+function getWeather(latitude, longitude) {
+    let api = `http://api.openweather.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+    fetch(api).then(function (response) {
+        let data = response.json();
+        return data;
+    })
+        .then(function (data) {
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function () {
+            displayWeather();
+        });
+}
+
+function displayWeather() {
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/`;
+    tempElement.innerHTML = `${weather.temperature.value}* <span>C</span>`;
+    DescElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
 }
