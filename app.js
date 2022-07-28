8ccec9b30ee393041398560cf30797a0096be033
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-07-28 22:56:24 +0100

    change key

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ function showError(error) {
 }
 
 function getWeather(latitude, longitude) {
-    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`;
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
     fetch(api).then(function (response) {
         let data = response.json();
